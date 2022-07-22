86fb8eaab0a659795c2a8640f86eaa62efaf1566
Ramesh1234569 <rameshbayya434@gmail.com>
2022-07-22 07:49:22 +0530

    This is my first code git

---
+++ mongptest.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://nagamani:<nagamani>@cluster0.ntzg6.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+    "name" : "ramesh",
+    "email": "ram@gmail.com",
+    "surname": "rb"
+}
+db1 = client['mongotest']
+coll = db1['test']
+
+
